23a1dd4377f92d61290d8c86d76048d34676ba07
Angrigo <gab.angrisani@gmail.com>
2019-03-23 19:24:43 +0100

    new version

---
+++ precache-manifest.90cfebff9943f3a0bb7b6b1951c3ecfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "babc212110b97db6b581",
+    "url": "/static/js/main.babc2121.chunk.js"
+  },
+  {
+    "revision": "bf43ceb232ab82b6a98c",
+    "url": "/static/js/1.bf43ceb2.chunk.js"
+  },
+  {
+    "revision": "babc212110b97db6b581",
+    "url": "/static/css/main.9af77896.chunk.css"
+  },
+  {
+    "revision": "bf43ceb232ab82b6a98c",
+    "url": "/static/css/1.0e91f726.chunk.css"
+  },
+  {
+    "revision": "d59f49548389097d5d962cd9f4c8c0cc",
+    "url": "/index.html"
+  }
+];
