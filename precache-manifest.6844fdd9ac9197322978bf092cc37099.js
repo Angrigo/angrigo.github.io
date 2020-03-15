b8ef384afa6c62ef2e0e0978f212ff13b5593588
sds <sd>
2020-03-15 18:45:54 +0100

    new site

---
+++ precache-manifest.6844fdd9ac9197322978bf092cc37099.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b47c39494b469e4f8d145a1ad580d02b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1825b1b9b9d6a8c5e3f4",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "abd826ff7feee2364ba6",
+    "url": "/static/css/main.d7be9dc5.chunk.css"
+  },
+  {
+    "revision": "1825b1b9b9d6a8c5e3f4",
+    "url": "/static/js/2.fa3df95d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.fa3df95d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd826ff7feee2364ba6",
+    "url": "/static/js/main.140ba291.chunk.js"
+  },
+  {
+    "revision": "c67e7fc494555f284ce1",
+    "url": "/static/js/runtime-main.a43b6eb2.js"
+  }
+]);
